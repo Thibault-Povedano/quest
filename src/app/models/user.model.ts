853647359ca3c94f66233c9a33d415bfd0a5c5ec
Thibault <thibault@mbp-de-thibault.home>
2022-06-29 11:44:33 +0200

    finish quest14

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+
+  constructor(
+    public userName: string,
+    public email: string,
+    public password: string,
+    public street :string,
+    public city :string,
+    public zipCode: string,
+  ){ }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +9,29 @@ import { FormControl } from '@angular/forms';
 })
 export class UserComponent implements OnInit {
 
-  username = new FormControl('');
+  user: User[] = [];
+
+  userName = new FormControl('');
+  mail = new FormControl('');
+  password = new FormControl('');
+  street = new FormControl('');
+  city = new FormControl('');
+  zipCode = new FormControl('');
+
+  subscription() {
+    this.user.push(new User(this.userName.value!,
+      this.mail.value!,
+      this.password.value!,
+      this.street.value!,
+      this.city.value!,
+      this.zipCode.value!))
+    console.log(this.user)
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log()
   }
-
 
 }
